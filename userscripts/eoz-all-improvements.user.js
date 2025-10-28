af76be63b759432f24812807d736c2ea276f1bdb
oski350bpm <lendzionoskar@gmail.com>
2025-10-28 13:28:06 +0100

    temp: Disable machines control panel module completely to test magazine mobile view - bump v3.5.6

--- userscripts/eoz-all-improvements.user.js
+++ userscripts/eoz-all-improvements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EOZ All UI Improvements
 // @namespace    https://github.com/oski350bpm/eoz-UI-improvements
-// @version      3.5.5
+// @version      3.5.6
 // @description  Wszystkie ulepszenia UI dla EOZ - loader dla wszystkich modułów (Boards + Veneers)
 // @match        https://eoz.iplyty.erozrys.pl/*
 // @updateURL    https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/eoz-all-improvements.user.js?v=1761642635
@@ -10,14 +10,13 @@
 // @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/eoz-header-menu.js?v=2.2.7
 // @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/eoz-commission-list-improvements.js?v=2.4.5
 // @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/eoz-boards-magazine-improvements.js?v=1761642634
-// @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/eoz-machines-control-panel-improvements.js?v=1761642635
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var VERSION = '3.5.5';
+    var VERSION = '3.5.6';
     
     if (!window.EOZ) {
         console.error('[EOZ All UI v' + VERSION + '] Core not loaded!');
