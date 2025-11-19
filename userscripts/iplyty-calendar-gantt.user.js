0df9cd328c9f12c7e229ae5a188618c8df92718a
oski350bpm <lendzionoskar@gmail.com>
2025-11-19 10:37:43 +0100

    feat: Comment out module import in Gantt Buffer Overlay - bump v1.0.3

--- userscripts/iplyty-calendar-gantt.user.js
+++ userscripts/iplyty-calendar-gantt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EOZ Gantt Buffer Overlay
 // @namespace    https://github.com/oski350bpm/eoz-UI-improvements
-// @version      1.0.2
+// @version      1.0.3
 // @description  Ułatwia planowanie zleceń z bufora na widoku Harmonogramu stanowisk (Gantt)
 // @match        https://iplyty.erozrys.pl/pl/new_machine/calendar_gantt*
 // @match        https://eoz.iplyty.erozrys.pl/pl/new_machine/calendar_gantt*
@@ -9,14 +9,14 @@
 // @updateURL    https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/iplyty-calendar-gantt.user.js
 // @downloadURL  https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/iplyty-calendar-gantt.user.js
 // @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/core/eoz-core.js
-// @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/iplyty-gantt-buffer.js
+// // @require      https://raw.githubusercontent.com/oski350bpm/eoz-UI-improvements/main/userscripts/modules/iplyty-gantt-buffer.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const VERSION = '1.0.2';
+    const VERSION = '1.0.3';
 
     if (!window.EOZ) {
         console.warn('[EOZ Gantt Buffer Overlay v' + VERSION + '] core not loaded');
